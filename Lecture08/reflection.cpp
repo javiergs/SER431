58155af9366cae1d5827b6719df629f494edb175
Javier <mscjaviergs@gmail.com>
2018-09-11 00:01:01 -0700

    Create reflection.cpp

---
+++ Lecture08/reflection.cpp
@@ -0,0 +1,532 @@
+/*
+Lecture 03
+http://javiergs/teaching/ser431
+javiergs@asu.edu
+*/
+
+#include <vector>
+#include <Imath/imathvec.h>
+#include <GL/glut.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include <windows.h>
+#include <wingdi.h>
+
+using namespace std;
+using namespace Imath;
+
+typedef Vec3<float> Vec3f;
+typedef Vec2<float> Vec2f;
+
+// mesh data structure
+struct Mesh {
+	// vertex
+	vector<Vec3f> dot_vertex;
+	vector<Vec3f> dot_normalPerFace;
+	vector<Vec3f> dot_normalPerVertex;
+	vector<Vec2f> dot_texture;
+	// faces
+	vector<int> face_index_vertex;
+	vector<int> face_index_normalPerFace;
+	vector<int> face_index_normalPerVertex;
+	vector<int> face_index_texture;
+};
+
+// global
+Mesh* mesh1;
+Mesh* mesh2;
+Mesh* mesh3;
+Mesh* mesh4;
+
+int width = 1200;
+int height = 600;
+float ratio = 1.0;
+GLuint display1, display2, display3, display4;
+
+// controling parameters
+int mouse_button;
+int mouse_x = 0;
+int mouse_y = 0;
+float scale = 0.3;
+float x_angle = 0.0;
+float y_angle = 0.0;
+
+// textures with BITMAPS
+GLuint textureArray[8];
+
+void loadBMP_custom(GLuint textureArray[], const char * imagepath, int n) {
+
+	printf("Reading image %s\n", imagepath);
+	// Data read from the header of the BMP file
+	unsigned char header[54];
+	unsigned int dataPos;
+	unsigned int imageSize;
+	unsigned int width, height;
+	unsigned char * data;
+
+	// Open the file
+	FILE * file = fopen(imagepath, "rb");
+	if (!file) {
+		printf("%s could not be opened. Are you in the right directory ? Don't forget to read the FAQ !\n", imagepath);
+		getchar();
+		return;
+	}
+	// If less than 54 bytes are read, problem
+	if (fread(header, 1, 54, file) != 54) {
+		printf("Not a correct BMP file\n");
+		fclose(file);
+		return;
+	}
+	// A BMP files always begins with "BM"
+	if (header[0] != 'B' || header[1] != 'M') {
+		printf("Not a correct BMP file\n");
+		fclose(file);
+		return;
+	}
+	// Make sure this is a 24bpp file
+	if (*(int*)&(header[0x1E]) != 0) { printf("Not a correct BMP file\n");    fclose(file); return; }
+	if (*(int*)&(header[0x1C]) != 24) { printf("Not a correct BMP file\n");    fclose(file); return; }
+	// Read the information about the image
+	dataPos = *(int*)&(header[0x0A]);
+	imageSize = *(int*)&(header[0x22]);
+	width = *(int*)&(header[0x12]);
+	height = *(int*)&(header[0x16]);
+	// Some BMP files are misformatted, guess missing information
+	if (imageSize == 0)    imageSize = width * height * 3; // 3 : one byte for each Red, Green and Blue component
+	if (dataPos == 0)      dataPos = 54; // The BMP header is done that way
+	data = new unsigned char[imageSize];
+	fread(data, 1, imageSize, file);
+	fclose(file);
+
+	// Create one OpenGL texture
+	glGenTextures(1, &textureArray[n]);
+	glBindTexture(GL_TEXTURE_2D, textureArray[n]);
+
+	glGenTextures(1, &textureArray[n]);
+	glBindTexture(GL_TEXTURE_2D, textureArray[n]);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // must set to 1 for compact data
+										   // glTexImage2D Whith size and minification
+	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_BGR_EXT, GL_UNSIGNED_BYTE, data);
+
+	delete[] data;
+
+	// Poor filtering, or ...
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); 
+	// ... nice trilinear filtering ...
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+}
+
+// create a triangulated cube
+Mesh* createCube() {
+	Mesh *mesh = new Mesh;
+	// Vertexes
+	int BOX_WIDTH = 100;
+	int BOX_HEIGHT = 100;
+	int BOX_DEPTH = 100;
+
+	mesh->dot_vertex.push_back(Vec3<GLfloat>(0.0, BOX_HEIGHT, BOX_DEPTH));
+	mesh->dot_vertex.push_back(Vec3<GLfloat>(BOX_WIDTH, BOX_HEIGHT, BOX_DEPTH));
+	mesh->dot_vertex.push_back(Vec3<GLfloat>(BOX_WIDTH, 0.0, BOX_DEPTH));
+	mesh->dot_vertex.push_back(Vec3<GLfloat>(0.0, 0.0, BOX_DEPTH));
+
+	mesh->dot_vertex.push_back(Vec3<GLfloat>(0.0, BOX_HEIGHT, 0.0));
+	mesh->dot_vertex.push_back(Vec3<GLfloat>(BOX_WIDTH, BOX_HEIGHT, 0.0));
+	mesh->dot_vertex.push_back(Vec3<GLfloat>(BOX_WIDTH, 0.0, 0.0));
+	mesh->dot_vertex.push_back(Vec3<GLfloat>(0.0, 0.0, 0.0));
+
+	// face 1
+	mesh->face_index_vertex.push_back(0);//0
+	mesh->face_index_vertex.push_back(2);//1
+	mesh->face_index_vertex.push_back(1);//2
+	mesh->face_index_vertex.push_back(0);//0
+	mesh->face_index_vertex.push_back(3);//2
+	mesh->face_index_vertex.push_back(2);//3
+										 // face 2
+	mesh->face_index_vertex.push_back(0);//0
+	mesh->face_index_vertex.push_back(5);//1
+	mesh->face_index_vertex.push_back(4);//5
+	mesh->face_index_vertex.push_back(0);//0
+	mesh->face_index_vertex.push_back(1);//5
+	mesh->face_index_vertex.push_back(5);//4
+										 // face 3
+	mesh->face_index_vertex.push_back(1);//1
+	mesh->face_index_vertex.push_back(6);//2
+	mesh->face_index_vertex.push_back(5);//6
+	mesh->face_index_vertex.push_back(1);//1
+	mesh->face_index_vertex.push_back(2);//6
+	mesh->face_index_vertex.push_back(6);//5
+										 // face 4
+	mesh->face_index_vertex.push_back(2);//2
+	mesh->face_index_vertex.push_back(7);//3
+	mesh->face_index_vertex.push_back(6);//7
+	mesh->face_index_vertex.push_back(2);//2
+	mesh->face_index_vertex.push_back(3);//7
+	mesh->face_index_vertex.push_back(7);//6
+										 // face 5
+	mesh->face_index_vertex.push_back(4);//0
+	mesh->face_index_vertex.push_back(3);//3
+	mesh->face_index_vertex.push_back(0);//7
+	mesh->face_index_vertex.push_back(4);//0
+	mesh->face_index_vertex.push_back(7);//7
+	mesh->face_index_vertex.push_back(3);//4
+										 // face 6
+	mesh->face_index_vertex.push_back(5);//4 
+	mesh->face_index_vertex.push_back(7);//5
+	mesh->face_index_vertex.push_back(4);//6
+	mesh->face_index_vertex.push_back(5);//4
+	mesh->face_index_vertex.push_back(6);//6
+	mesh->face_index_vertex.push_back(7);//7
+
+										 // texture
+	mesh->dot_texture.push_back(Vec2<GLfloat>(0.0, 1.0));
+	mesh->dot_texture.push_back(Vec2<GLfloat>(1.0, 1.0));
+	mesh->dot_texture.push_back(Vec2<GLfloat>(1.0, 0.0));
+	mesh->dot_texture.push_back(Vec2<GLfloat>(0.0, 0.0));
+	for (int t = 0; t<6; t++) {
+		mesh->face_index_texture.push_back(0);//0
+		mesh->face_index_texture.push_back(2);//1
+		mesh->face_index_texture.push_back(1);//2
+		mesh->face_index_texture.push_back(0);//0
+		mesh->face_index_texture.push_back(3);//2
+		mesh->face_index_texture.push_back(2);//3
+	}
+
+	return mesh;
+}
+
+// creating a triangulated plane
+Mesh* createPlane(int arena_width, int arena_depth, int arena_cell) {
+	Mesh *me = new Mesh;
+	int n = arena_width / arena_cell;
+	int m = arena_depth / arena_cell;
+
+	// vertices
+	for (int i = 0; i<n; i++) {
+		for (int j = 0; j < m; j++) {
+			me->dot_vertex.push_back(Vec3<GLfloat>(i*arena_cell, 0.0, j*arena_cell));
+		}
+	}
+	//texture
+	me->dot_texture.push_back(Vec2<GLfloat>(0.0, 0.0));
+	me->dot_texture.push_back(Vec2<GLfloat>(0.0, 1.0));
+	me->dot_texture.push_back(Vec2<GLfloat>(1.0, 0.0));
+	me->dot_texture.push_back(Vec2<GLfloat>(1.0, 1.0));
+	// faces
+	for (int i = 0; i<(n*m) - m; i++) {
+		if ((i + 1) % n == 0) continue;
+		me->face_index_vertex.push_back(i); me->face_index_vertex.push_back(i + 1);
+		me->face_index_vertex.push_back(i + n);
+		me->face_index_vertex.push_back(i + 1); me->face_index_vertex.push_back(i + n + 1);
+		me->face_index_vertex.push_back(i + n);
+		// index for texture
+		for (int t = 0; t<6; t++) {
+			me->face_index_texture.push_back(3);//0
+			me->face_index_texture.push_back(2);//2
+			me->face_index_texture.push_back(1);//1
+			me->face_index_texture.push_back(2);//0
+			me->face_index_texture.push_back(0);//3
+			me->face_index_texture.push_back(1);//2
+		}
+	}
+	return me;
+}
+// normal per face
+void calculateNormalPerFace(Mesh* m) {
+	Vec3<float> v1, v2, v3, v4, v5;
+	for (int i = 0; i < m->face_index_vertex.size(); i += 3) {
+		v1 = m->dot_vertex[m->face_index_vertex[i]];
+		v2 = m->dot_vertex[m->face_index_vertex[i + 1]];
+		v3 = m->dot_vertex[m->face_index_vertex[i + 2]];
+		v4 = (v2 - v1);
+		v5 = (v3 - v1);
+		v4 = v4.cross(v5);
+		v4.normalize();
+		m->dot_normalPerFace.push_back(v4);
+		int pos = m->dot_normalPerFace.size() - 1;
+		// same normal for all vertex in this face
+		m->face_index_normalPerFace.push_back(pos);
+		m->face_index_normalPerFace.push_back(pos);
+		m->face_index_normalPerFace.push_back(pos);
+	}
+}
+
+// calculate normal per vertex
+void calculateNormalPerVertex(Mesh* m) {
+	m->dot_normalPerVertex.clear();
+	m->face_index_normalPerVertex.clear();
+	Vec3<float> suma; suma.x = 0; suma.y = 0; suma.z = 0;
+	//initialize
+	for (unsigned int val = 0; val < m->dot_vertex.size(); val++) {
+		m->dot_normalPerVertex.push_back(suma);
+	}
+	// calculate sum for vertex
+	for (long pos = 0; pos < m->face_index_vertex.size(); pos++) {
+		m->dot_normalPerVertex[m->face_index_vertex[pos]] +=
+			m->dot_normalPerFace[m->face_index_normalPerFace[pos]];
+	}
+	// normalize for vertex 
+	for (unsigned int val = 0; val < m->dot_normalPerVertex.size(); val++) {
+		m->dot_normalPerVertex[val] = m->dot_normalPerVertex[val].normalize();
+	}
+	//normalVertexIndex is the same that vertexIndex
+	for (unsigned int pos = 0; pos < m->face_index_vertex.size(); pos++) {
+		m->face_index_normalPerVertex.push_back(m->face_index_vertex[pos]);
+	}
+}
+
+// draw
+GLuint meshToDisplayList(Mesh* m, int id, int textureId) {
+	GLuint listID = glGenLists(id);
+	glNewList(listID, GL_COMPILE);
+	//if (id != 3) {
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glBindTexture(GL_TEXTURE_2D, textureArray[textureId]);
+	//}
+	glBegin(GL_TRIANGLES);
+	for (unsigned int i = 0; i < m->face_index_vertex.size(); i++) {
+		// PER VERTEX NORMALS
+		if ((!m->dot_normalPerVertex.empty() && !m->face_index_normalPerVertex.empty())) {
+			glNormal3fv(&m->dot_normalPerVertex[m->face_index_normalPerVertex[i]].x);
+		}
+		// TEXTURES
+		if (!m->dot_texture.empty() && !m->face_index_texture.empty()) {
+			glTexCoord2fv(&m->dot_texture[m->face_index_texture[i]].x);
+		}
+		// COLOR
+		//Vec3f offset = (m->dot_vertex[m->face_index_vertex[i]]);
+		// VERTEX
+		//glColor3f(fabs(sin(offset.x)), fabs(cos(offset.y)), fabs(offset.z));
+		glVertex3fv(&m->dot_vertex[m->face_index_vertex[i]].x);
+	}
+	glEnd();
+	if (id != 3) {
+		glDisable(GL_TEXTURE_2D);
+	}
+	glEndList();
+	return listID;
+}
+
+// reshape
+void reshape(int w, int h) {
+	width = w;
+	height = h;
+	if (h == 0) h = 1;
+	ratio = 1.0f * w / h;
+}
+
+// mouse
+void mouse(int button, int state, int x, int y) {
+	mouse_x = x;
+	mouse_y = y;
+	mouse_button = button;
+}
+
+// motion
+void motion(int x, int y) {
+	if (mouse_button == GLUT_LEFT_BUTTON) {
+		y_angle += (float(x - mouse_x) / width) *360.0;
+		x_angle += (float(y - mouse_y) / height)*360.0;
+	}
+	if (mouse_button == GLUT_RIGHT_BUTTON) {
+		scale += (y - mouse_y) / 100.0;
+		if (scale < 0.1) scale = 0.1;
+		if (scale > 7)	scale = 7;
+	}
+	mouse_x = x;
+	mouse_y = y;
+	glutPostRedisplay();
+}
+
+// text
+void renderBitmapString(float x, float y, float z, const char *string) {
+	const char *c;
+	glRasterPos3f(x, y, z);   // fonts position
+	for (c = string; *c != '\0'; c++)
+		glutBitmapCharacter(GLUT_BITMAP_8_BY_13, *c);
+}
+
+// init
+void init() {
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_DEPTH_TEST);
+	ratio = (double)width / (double)height;
+	// mesh
+	mesh1 = createPlane(2000, 2000, 100);
+	mesh2 = createCube();
+	mesh3 = createCube();
+	mesh4 = createCube();
+	calculateNormalPerFace(mesh1);
+	calculateNormalPerFace(mesh2);
+	calculateNormalPerFace(mesh3);
+	calculateNormalPerFace(mesh4);
+	calculateNormalPerVertex(mesh1);
+	calculateNormalPerVertex(mesh2);
+	calculateNormalPerVertex(mesh3);
+	calculateNormalPerVertex(mesh4);
+	// textures
+	loadBMP_custom(textureArray, "../../BMP files/brick.bmp", 0);
+	loadBMP_custom(textureArray, "../../BMP files/oldbox.bmp", 1);
+
+	display1 = meshToDisplayList(mesh1, 1, 0);
+	display2 = meshToDisplayList(mesh2, 2, 1);
+	display3 = meshToDisplayList(mesh3, 3, 1);
+	display4 = meshToDisplayList(mesh4, 4, 1);
+	// light
+	GLfloat light_ambient[] = { 0.5, 0.5, 0.5, 1.0 };
+	GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light_position[] = { 0.0, 0.0, 1.0, 0.0 };
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+
+	// STENCIL|STEP 2. NEW LINES
+	//glEnable(GL_BLEND);
+	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glClearStencil(0); //define the value to use as clean.
+
+}
+
+// display
+void display(void) {
+	// STENCIL-STEP 3. enable and configure
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+	
+	// projection
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	glViewport(0, 0, width, height);
+	gluPerspective(45, ratio, 1, 1000);
+	// view
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+	// lookAt
+	gluLookAt(0.0f, 40.0f, 320.0,
+		0.0f, 1.0f, -1.0f,
+		0.0f, 1.0f, 0.0f);
+	// camera
+	glScalef(scale, scale, scale);
+	glRotatef(x_angle, 1.0f, 0.0f, 0.0f);
+	glRotatef(y_angle, 0.0f, 1.0f, 0.0f);
+	glTranslatef(0.0f, 0.0f, 0.0f);
+
+	// ===== STENCIL DRAW ============================================================================
+
+	glEnable(GL_STENCIL_TEST); //Start using the stencil
+	  glDisable(GL_DEPTH_TEST);
+	  glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE); //Disable writing colors in frame buffer
+	  glStencilFunc(GL_ALWAYS, 1, 0xFFFFFFFF); //Place a 1 where rendered
+	  glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE); 	//Replace where rendered
+	  // PLAIN for the stencil
+	  glPushMatrix();
+	  glTranslatef(-900, 0, -900);
+	  glCallList(display1);
+	  glPopMatrix();
+	  glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE); //Reenable color
+	glEnable(GL_DEPTH_TEST);
+	glStencilFunc(GL_EQUAL, 1, 0xFFFFFFFF);
+	glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP); //Keep the pixel
+
+
+
+	// boxes reflections
+	glPushMatrix();
+	glScalef(1.0, -1.0, 1.0);
+	glCallList(display2);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(200, 0, 0);
+	glScalef(1.0, -1.0, 1.0);
+	glCallList(display3);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(-200, 0, 0);
+	glScalef(1.0, -1.0, 1.0);
+	glCallList(display4);
+	glPopMatrix();
+
+
+	// STENCIL-STEP 4. disable it
+	glDisable(GL_STENCIL_TEST);
+	
+	// ===== 3D ENVIRONMENT AS USUAL ==================================================================
+
+	//plane
+	glEnable(GL_BLEND);
+	glDisable(GL_LIGHTING);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glColor4f(0.7, 0.0, 0.0, 0.3);
+	glColor4f(1.0, 1.0, 1.0, 0.3);
+	glPushMatrix();
+	glTranslatef(-900, 0, -900);
+	glCallList(display1);
+	glPopMatrix();
+	glEnable(GL_LIGHTING);
+	glDisable(GL_BLEND);
+	// box 1
+	glPushMatrix();
+	glCallList(display2);
+	glPopMatrix();
+	// box 2
+	glPushMatrix();
+	glTranslatef(200, 0, 0);
+	glCallList(display3);
+	glPopMatrix();
+	// box 3
+	glPushMatrix();
+	glTranslatef(-200, 0, 0);
+	glCallList(display4);
+	glPopMatrix();
+	// end
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+	// texto
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0, width, 0, height);
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+	glColor3f(1.0, 1.0, 1.0);
+	renderBitmapString(0.0, height - 13.0f, 0.0f, "Use [Mouse Left Key] to rotate");
+	renderBitmapString(0.0, height - 26.0f, 0.0f, "Use [Mouse Right Key] to scale");
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+
+	//
+	glutSwapBuffers();
+}
+
+// main
+void main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	// STENCIL-STEP 1. init
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA | GLUT_STENCIL);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(width, height);
+	glutCreateWindow("Stencil Buffer");
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+	glutMouseFunc(mouse);
+	glutMotionFunc(motion);
+	init();
+	glutMainLoop();
+}
